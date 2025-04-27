1986306dcdc0e7dea2b1a70f8c4eab3bedb6525b
Felipe Souza <125583116+FelipeSouza6k@users.noreply.github.com>
2025-04-27 20:43:59 -0300

    Add files via upload

---
+++ Aula07/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Aula07;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Operadores de atribuição\n");
+        int a = 10;
+        Console.WriteLine($"a = {a}");
+
+        //Operador de atribuição
+        a = 20;
+        Console.WriteLine($"\na = {a}");
+        //Operador (++)
+        a++; // a = 21
+        Console.WriteLine($"\na = {a}");
+        //(a+=)
+        a += 10; // a = 31
+        Console.WriteLine($"\na = {a}");
+
+        a-= 10; // a = 21
+        Console.WriteLine($"\na = {a}");
+
+    }
+}
