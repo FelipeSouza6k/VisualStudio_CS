aaaa02c63b5976dafa02f9e2e25d9bbe18cbef9c
FelipeSouza6k <125583116+FelipeSouza6k@users.noreply.github.com>
2025-04-28 18:08:09 -0300

    Aula08

---
+++ Aula08/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Aula08;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Operadores Relacionais\n");
+        Console.WriteLine("Digite o Primeiro número\n");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite o Segundo número\n");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        //Operador de Igualdade (==)
+        if (x == y)
+        {
+            Console.WriteLine("Os números são iguais\n");
+        }
+        //Operador de Diferença (!=) 
+        if (x != y)
+        {
+            Console.WriteLine("Os números são diferentes\n");
+        }
+        //Operador Menor que
+        if (x < y)
+        {
+            Console.WriteLine("X é menor que Y\n");
+        }
+        //Operador Maior que
+        if (x > y)
+        {
+            Console.WriteLine("X é maior que Y\n");
+        }
+        //Operador Menor ou igual a
+        if (x <= y)
+        {
+            Console.WriteLine("X é menor ou igual a Y\n");
+        }
+        //Operador Maior ou igual a
+        if (x >= y)
+        {
+            Console.WriteLine("X é maior ou igual a Y\n");
+        }
+
+    }
+}
+
