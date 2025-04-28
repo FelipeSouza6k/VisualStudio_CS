b6000a8117464ac464eb6e5b3f36fde22b3088b1
FelipeSouza6k <125583116+FelipeSouza6k@users.noreply.github.com>
2025-04-28 18:20:23 -0300

    Aula9

---
+++ Aula09/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Aula09;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Operadores Lógicos");
+
+        bool isLogged = true;
+        bool hasAdminAcess = false;
+
+        Console.WriteLine("Informações do Usúario");
+        Console.WriteLine($"Logado: {isLogged}");
+        Console.WriteLine($"Acesso Admin: {hasAdminAcess}");
+        //Operador Logico AND (&&)
+        if (isLogged && hasAdminAcess)
+        {
+            Console.WriteLine("Acesso ao sistema");
+        }
+        else
+        {
+            Console.WriteLine("Acesso negado");
+        }
+
+        //Operador Logico OR (||)
+        if (isLogged || hasAdminAcess)
+        {
+            Console.WriteLine("Acesso ao sistema concedido");
+        }
+        else
+        {
+            Console.WriteLine("Acesso negado");
+        }
+        if (!isLogged)
+        {
+            Console.WriteLine("Usuário não logado");
+        }
+        else
+        {
+            Console.WriteLine("Usuário logado");
+        }
+    }
+}
