c3e19120c6d69be4e1d42b0329494ee67f04b1be
FelipeSouza6k <125583116+FelipeSouza6k@users.noreply.github.com>
2025-05-03 20:41:40 -0300

    Aula 1, Utilizando foreach em listas

---
+++ Aula15/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        //Coleções (listas)
+        List<int> numberList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        foreach(int number in numberList)
+        {
+            Console.WriteLine(number);
+
+        }
+        List<string> stringList = new List<string>() { "Rodrigo", "Felipe", "Valkiria", "Sabonete", "Luan", "Porta" };
+        foreach(string lista in stringList)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+}
