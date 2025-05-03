6c04e5d5f462eca74dbf3923d4f37706c3d571b4
FelipeSouza6k <125583116+FelipeSouza6k@users.noreply.github.com>
2025-05-03 20:30:53 -0300

    TEMPORIZADOR UTILIZANDO LAÇO DE REPETIÇÃO FOR

---
+++ Aula14/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("TEMPORIZADOR");
+        Console.WriteLine("Digite o tempo em horas: ");
+
+        int horas = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite o tempo em minutos: ");
+
+        int minutos = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite o tempo em segundos: ");
+
+        int segundos = Convert.ToInt32(Console.ReadLine());
+
+
+        for (int decrementoHoras = horas;  horas > -1; horas--)
+        {
+            for (int decrementoMinutos = minutos; minutos > -1; minutos--)
+            {
+                for (int decrementoSegundos = segundos; segundos > -1; segundos--)
+                {
+                    Console.WriteLine($"{horas} : {minutos} : {segundos}");
+                }
+                segundos = 59;
+            }
+            minutos = 59;
+        }
+    }
+}
