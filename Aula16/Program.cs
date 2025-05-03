97f0acecdd59f702b49289d8bfc9967acd783475
FelipeSouza6k <125583116+FelipeSouza6k@users.noreply.github.com>
2025-05-03 20:47:39 -0300

    Utilizando arrays, e imprimindo valores de 2 maneiras diferentes

---
+++ Aula16/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        //Declaração de um array de strings
+        string[] fruits = { "apple", "banana", "cherry", "date", "elderberry" };
+
+        Console.WriteLine(fruits[1] + "\n\n\n\n\n\n");
+
+        Console.WriteLine("Utilizando o foreach(abaixo)");
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+    }
+}
