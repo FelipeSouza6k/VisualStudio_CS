3681fcf8942c769ecede405770ca1ce2ecbc6cad
FelipeSouza6k <125583116+FelipeSouza6k@users.noreply.github.com>
2025-05-03 20:17:20 -0300

    Aula12

---
+++ Aula12/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        //       int i = 0;
+        //     while(i <= 5){
+        //       Console.WriteLine(i);
+        //     i++;
+        //}
+        int bullet = 100;
+        while (bullet > 0)
+        {
+            Console.WriteLine("Você tem " + bullet + " munições retantes.");
+            bullet--;
+        }
+    }
+}
